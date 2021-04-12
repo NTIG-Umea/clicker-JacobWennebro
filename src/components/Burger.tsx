e3ecaccbad05985ba1981b3f83cde29ff5cbd729
Jacob Wennebro <jacob.wennebro@learnet.se>
2021-04-12 11:13:33 +0200

    Uppdaterade med ljud effekt

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -1,15 +1,24 @@
-import React, { Component, MouseEvent, RefObject } from 'react'
+import React, { Component, createRef, MouseEvent, RefObject } from 'react'
 
 interface props {onClick: (e: MouseEvent<HTMLDivElement>) => void, upgrades: ["patty", "lettuce", "cheese"]}
 
 export default class Burger extends Component<props> {
-    
+    private Burger = createRef() as RefObject<HTMLDivElement>
+    private sound = new Audio("/clicker-JacobWennebro/click.mp3");
+
     render() {
+
+        const mouseDown = () => {
+            if(this.Burger.current) this.Burger.current.style.transform = "scale(1.1) translateY(-50%)";
+            this.sound.currentTime = 0.1;
+            this.sound.play();
+        }
+
         return (
-            <div onClick={this.props.onClick} className="burger">
+            <div ref={this.Burger} onClick={this.props.onClick} onMouseDown={mouseDown} onMouseUp={() => this.Burger.current ? this.Burger.current.style.transform = "" : ""} className="burger">
                 <div className="burger__bun top burger-item"></div>
 
-                {this.props.upgrades.reverse().slice(this.props.upgrades.length-25, this.props.upgrades.length).reverse().map((i => 
+                {this.props.upgrades.reverse().slice(this.props.upgrades.length-25, this.props.upgrades.length).map((i => 
                     <div className={`burger__${i} burger-item`}></div>
                 ))}
 
